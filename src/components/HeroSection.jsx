df366bc603daf1921b26206bb6189adba7c2088e
CODE-SAMRAT <paharisamrat@gmail.com>
2024-11-03 02:32:59 +0545

    Nav and Hero Section Completed

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -1,5 +1,7 @@
 import React, { useEffect, useState } from "react";
 import { heroVideo, smallHeroVideo } from "../utils";
+import { useGSAP } from "@gsap/react";
+import { gsap } from "gsap";
 
 const HeroSection = () => {
   const [videoSrc, setVideoSrc] = useState(
@@ -12,7 +14,17 @@ const HeroSection = () => {
       setVideoSrc(heroVideo);
     }
   };
-
+  useGSAP(() => {
+    gsap.to("#hero", {
+      opacity: 1,
+      duration: 2.1,
+    });
+    gsap.to("#cta", {
+      opacity: 1,
+      y: -50,
+      delay: 2.1,
+    });
+  }, []);
   useEffect(() => {
     window.addEventListener("resize", handleVideoSrcSet);
     return window.removeEventListener("resize", handleVideoSrcSet);
@@ -34,6 +46,15 @@ const HeroSection = () => {
             <source src={videoSrc} type="video/mp4" />
           </video>
         </div>
+      </div>
+      <div
+        id="cta"
+        className="flex flex-col items-center opacity-0 translate-y-20"
+      >
+        <a href="#highlights" className="btn">
+          Buy
+        </a>
+        <p className="font-normal text-xl">From $199/month ot $999</p>
       </div>
     </section>
   );
