2d1e7988418e8e841b48435a279d275d2ea3d62b
CODE-SAMRAT <paharisamrat@gmail.com>
2024-11-03 01:47:55 +0545

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,15 @@
-import Navbar from './components/Navbar';
-import Hero from './components/Hero';
-import Highlights from './components/Highlights';
-import Model from './components/Model';
-import Features from './components/Features';
-import HowItWorks from './components/HowItWorks';
-import Footer from './components/Footer';
-
-import * as Sentry from '@sentry/react';
+import HeroSection from "./components/HeroSection";
+import Hightlight from "./components/Hightlight";
+import Navbar from "./components/Navbar";
 
 const App = () => {
   return (
-    <main className="bg-black">
+    <main className=" bg-black">
       <Navbar />
-      <Hero />
-      <Highlights />
-      <Model />
-      <Features />
-      <HowItWorks />
-      <Footer />
+      <HeroSection />
+      <Hightlight />
     </main>
-  )
-}
+  );
+};
 
-export default Sentry.withProfiler(App);
+export default App;
---
+++ src/components/Hightlight.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Hightlight = () => {
+  return <div>Highlight</div>;
+};
+
+export default Hightlight;
