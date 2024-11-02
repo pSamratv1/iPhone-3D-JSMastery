371ccba47db15867a822632f7d5f564b37275be1
CODE-SAMRAT <paharisamrat@gmail.com>
2024-11-03 02:21:11 +0545

    Navbar and herosection in progress

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -1,7 +1,42 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { heroVideo, smallHeroVideo } from "../utils";
 
 const HeroSection = () => {
-  return <div>HeroSection</div>;
+  const [videoSrc, setVideoSrc] = useState(
+    window.innerWidth < 760 ? smallHeroVideo : heroVideo
+  );
+  const handleVideoSrcSet = () => {
+    if (window.innerWidth < 760) {
+      setVideoSrc(smallHeroVideo);
+    } else {
+      setVideoSrc(heroVideo);
+    }
+  };
+
+  useEffect(() => {
+    window.addEventListener("resize", handleVideoSrcSet);
+    return window.removeEventListener("resize", handleVideoSrcSet);
+  }, []);
+  return (
+    <section className="w-full nav-height bg-black">
+      <div className="h-5/6 w-full flex-center flex-col">
+        <p id="hero" className="hero-title">
+          iPhone 15 Pro
+        </p>
+        <div className="md:w-10/12 w-9/12">
+          <video
+            className="pointer-event-none"
+            autoPlay
+            muted
+            playsInline={true}
+            key={videoSrc}
+          >
+            <source src={videoSrc} type="video/mp4" />
+          </video>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default HeroSection;
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,15 +4,20 @@ import { navLists } from "../constants";
 
 const Navbar = () => {
   return (
-    <header className="w-full py-4 px-5 flex justify-between">
+    <header className="w-full py-5 px-5 sm:px-10 flex justify-between">
       <nav className="flex w-full screen-max-width justify-between">
         <img src={appleImg} alt="Apple Logo" width={14} height={18} />
         <div className="flex flex-1 justify-center max-sm:hidden">
           {navLists.map((nav, id) => (
-            <div key={id}>{nav}</div>
+            <div
+              key={id}
+              className="px-5 text-sm cursor-pointer text-gray hover:text-white transition-all"
+            >
+              {nav}
+            </div>
           ))}
         </div>
-        <div>
+        <div className="flex items-baseline gap-7 max-sm:flex-1 max-sm:justify-end">
           <img src={searchImg} alt="Search" width={18} height={18} />
           <img src={bagImg} alt="Store" width={18} height={18} />
         </div>
