c33123e1e0994679a35186f549ff619cf7e380cb
CODE-SAMRAT <paharisamrat@gmail.com>
2024-11-06 16:43:00 +0545

    Model Successfully loaded

--- src/components/Model.jsx
+++ src/components/Model.jsx
@@ -1,6 +1,6 @@
 import { useGSAP } from "@gsap/react";
 import { gsap } from "gsap";
-import React, { useRef, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import ModelView from "./ModelView";
 import { yellowImg } from "../utils";
 import * as THREE from "three";
@@ -8,6 +8,7 @@ import { Canvas, useThree } from "@react-three/fiber";
 import { Group } from "three";
 import { View } from "@react-three/drei";
 import { models, sizes } from "../constants";
+import { animateWithGsapTimeline } from "../utils/animations";
 
 const Model = () => {
   const [size, setSize] = useState("small");
@@ -27,6 +28,22 @@ const Model = () => {
   // Rotation
   const [smallRotation, setSmallRotation] = useState(0);
   const [largeRotation, setLargeRotation] = useState(0);
+
+  const tl = gsap.timeline();
+  useEffect(() => {
+    if (size === "large") {
+      animateWithGsapTimeline(tl, small, smallRotation, "#view1", "#view2", {
+        transform: "translateX(-100%)",
+        duration: 2,
+      });
+    }
+    if (size === "small") {
+      animateWithGsapTimeline(tl, large, largeRotation, "#view2", "#view1", {
+        transform: "translateX(0)",
+        duration: 2,
+      });
+    }
+  }, [size]);
 
   useGSAP(() => {
     gsap.to("#heading", { opacity: 1, y: 0 });
--- src/components/ModelView.jsx
+++ src/components/ModelView.jsx
@@ -1,13 +1,21 @@
-import { PerspectiveCamera, View } from "@react-three/drei";
+import {
+  Html,
+  OrbitControls,
+  PerspectiveCamera,
+  View,
+} from "@react-three/drei";
+import * as THREE from "three";
 import React, { Suspense } from "react";
 import Lights from "./Lights";
+import IPhone from "./IPhone";
+import Loader from "./Loader";
 
 const ModelView = ({
   index,
   gropuRef,
   gsapType,
   controlRef,
-  setRotationSize,
+  setRotationState,
   size,
   item,
 }) => {
@@ -22,7 +30,28 @@ const ModelView = ({
       <ambientLight intensity={0.3} />
       <PerspectiveCamera makeDefault position={[0, 0, 4]} />
       <Lights />
-      <Suspense fallback={<div>Loading</div>}></Suspense>
+      <OrbitControls
+        makeDefault
+        ref={controlRef}
+        enableZoom={false}
+        enablePan={false}
+        rotateSpeed={0.4}
+        target={new THREE.Vector3(0, 0, 0)}
+        onEnd={() => setRotationState(controlRef.current.getAzimuthalAngle())}
+      />
+      <group
+        ref={gropuRef}
+        name={`${index === 1}? 'small':'large'`}
+        position={[0, 0, 0]}
+      >
+        <Suspense fallback={<Loader />}>
+          <IPhone
+            scale={index === 1 ? [15, 15, 15] : [17, 17, 17]}
+            item={item}
+            size={size}
+          />
+        </Suspense>
+      </group>
     </View>
   );
 };
