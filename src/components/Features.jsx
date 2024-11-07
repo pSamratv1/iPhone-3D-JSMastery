36dafc3a8990c76104c3b5e52c0cc6aa655f1381
CODE-SAMRAT <paharisamrat@gmail.com>
2024-11-07 23:16:17 +0545

    Feature Section about complete

--- src/components/Features.jsx
+++ src/components/Features.jsx
@@ -2,10 +2,39 @@ import { useGSAP } from "@gsap/react";
 
 import React from "react";
 import { animateWithGsap } from "../utils/animations";
+import { explore1Img, explore2Img, exploreVideo } from "../utils";
+import { useRef } from "react";
+import { gsap } from "gsap";
 
 const Features = () => {
+  const videoRef = useRef();
   useGSAP(() => {
+    gsap.to("#exporeVideo", {
+      scrollTrigger: {
+        trigger: "#exporeVideo",
+        toggleActions: "play pause restart reverse",
+        start: "-10% bottom",
+      },
+      onComplete: () => {
+        video.play();
+      },
+    });
     animateWithGsap("#features_title", { y: 0, opacity: 1 });
+    animateWithGsap(
+      ".g_grow",
+      {
+        scale: 1,
+        opacity: 1,
+        ease: "power1",
+      },
+      { scrub: 5.5 }
+    );
+    animateWithGsap(".g_text", {
+      y: 0,
+      opacity: 1,
+      ease: "power.inOut",
+      duration: 1,
+    });
   }, []);
 
   return (
@@ -23,8 +52,61 @@ const Features = () => {
               Forged in titanium
             </h2>
           </div>
-          <div className="flex-center flex-col px-10">
-            <div className="relative h-[50vh] w-full flex items-center"></div>
+          <div className="flex-center flex-col sm:px-10">
+            <div className="relative h-[50vh] w-full flex items-center">
+              <video
+                ref={videoRef}
+                playsInline
+                id="exploreVideo"
+                className="w-full h-full object-center"
+                preload="none"
+                autoPlay
+                muted
+              >
+                <source src={exploreVideo} type="video/mp4" />
+              </video>
+            </div>
+            <div className="flex flex-col w-full relative">
+              <div className="feature-video-container">
+                <div className="overlfow-hidden flex-1 h-[50vh]">
+                  <img
+                    src={explore1Img}
+                    alt="titanium"
+                    className="feature-vide0 g_grow"
+                  />
+                </div>
+                <div className="overflow-hidden flex-1 h-[50vh]">
+                  <img
+                    src={explore2Img}
+                    alt="titanium 2"
+                    className="feature-vide0 g_grow"
+                  />
+                </div>
+              </div>
+              <div className="feature-text-container">
+                <div className="flex-1 flex-center">
+                  <p className="feature-text g_text">
+                    iphone 15 Pro is{" "}
+                    <span className="text-white">
+                      the first iPhone to feature an aerospace-grade titanium
+                      design
+                    </span>
+                    using the same alloy that spacecraft use for missions to
+                    Mars.
+                  </p>
+                </div>
+                <div className="flex-1 flex-center">
+                  <p className="feature-text g_text">
+                    Titanium has one of the best strength-to-weight ratios of
+                    any metal,making these our{" "}
+                    <span className="text-white">
+                      lightest Pro models ever.
+                    </span>
+                    You'll notice the difference the moment you pick one up.
+                  </p>
+                </div>
+              </div>
+            </div>
           </div>
         </div>
       </div>
