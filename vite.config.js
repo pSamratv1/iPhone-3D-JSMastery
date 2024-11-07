d48700180d940fce0a1650493c82963613e9c93e
CODE-SAMRAT <paharisamrat@gmail.com>
2024-11-08 01:07:29 +0545

    Pages publishing

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,13 @@
 import { sentryVitePlugin } from "@sentry/vite-plugin";
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tailwindcss from "tailwindcss";
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), sentryVitePlugin({
-    org: "jsm-x9",
-    project: "javascript-react"
-  })],
+  plugins: [react(), tailwindcss()],
+  base: "/iPhone-3D-JSMastery/",
 
   build: {
-    sourcemap: true
-  }
-})
+    sourcemap: true,
+  },
+});
